c992524d48048b07583a82b2c1e6af6b473e4f9f
gitUserHam <shockwave_peter@hotmail.com>
2015-03-19 22:07:20 +1000

    Current files as of 19/03/2015

---
+++ movement/Assets/teleport_y.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleport_y : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D player){
+		player.transform.position = new Vector2( player.attachedRigidbody.position.x, -(player.attachedRigidbody.position.y+2));
+		player.attachedRigidbody.velocity = new Vector2(player.attachedRigidbody.velocity.x, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
